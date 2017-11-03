9cda2edd2d048a8acdc2aa955697e8f9940aa2dc
Boran Wen <aimozhe@gmail.com>
2017-11-03 11:17:12 -0400

    Update 347TopFrequentElements.java

--- 347TopFrequentElements.java
+++ 347TopFrequentElements.java
@@ -1,18 +1,17 @@
-
 class Solution {
     public List<Integer> topKFrequent(int[] nums, int k) {
-        List<Integer>[] buckets = new List[nums.length + 1];  // 3
+        List<Integer>[] buckets = new List[nums.length + 1];  
         Map<Integer, Integer> map = new HashMap<>();
         for (int num : nums) 
-            map.put(num, map.getOrDefault(num, 0) + 1);    // 6
+            map.put(num, map.getOrDefault(num, 0) + 1);    
         for (int key : map.keySet()) {
             int freq = map.get(key);
-            if (buckets[freq] == null)                      // 9
-                buckets[freq] = new ArrayList<Integer>();   // 10
+            if (buckets[freq] == null)                     
+                buckets[freq] = new ArrayList<Integer>();  
             buckets[freq].add(key);
         }
         List<Integer> list = new ArrayList<>();
-        for (int i = nums.length; i >= 0 && k > 0; i--) {     // 16
+        for (int i = nums.length; i >= 0 && k > 0; i--) {    
             List<Integer> l = buckets[i];
             while (l != null && l.size() > 0 && k > 0) {
                 list.add(l.get(l.size() - 1));
@@ -23,7 +22,7 @@ class Solution {
         return list;
     }
 }
-// 注意15行，是不是null和大小是不是0是两个概念，要分开判断，
-// 注意第2行如何声明一个元素为list的数组，
-// 注意第5行如何不用判断元素是否存在于map中而添加元素，
-// 第8，9行不能少，少了会有nullpointer异常
+// 注意14行，是不是null和大小是不是0是两个概念，要分开判断，
+// 注意第3行如何声明一个元素为list的数组，
+// 注意第6行如何不用判断元素是否存在于map中而添加元素，
+// 第9，10行不能少，少了会有nullpointer异常
